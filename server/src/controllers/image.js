ec5d105383c20b869a19b5b28a817d23a0278380
Alok <alok.sharma61630@gmail.com>
2021-05-09 15:40:06 +0530

    Image controller bug resolved

--- server/src/controllers/image.js
+++ server/src/controllers/image.js
@@ -1,9 +1,19 @@
 const asyncHandler = require("express-async-handler");
+const Product = require("../models/product");
 const Image = require("../models/image");
 
 const uploadImageForProduct = asyncHandler(async (req, res) => {
 	const { isPrimary } = req.body;
 	const { productId } = req.params;
+
+	const product = await Product.findById(productId);
+	if (!product) {
+		return res.status(400).send({
+			success: false,
+			data: null,
+			message: "Product doesn't exist",
+		});
+	}
 
 	const image = new Image({
 		url: req.file.path,
